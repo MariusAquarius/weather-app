35c64de049e5a26e176605ff361330fbc3a6b5dd
Marius Unger <me@marius-unger.de>
2024-11-05 21:41:28 +0100

    switch to HashRouter for static hosting

--- src/lib/router/router.tsx
+++ src/lib/router/router.tsx
@@ -1,12 +1,12 @@
 import {
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
   Route,
 } from "react-router-dom"
 import { ROUTES } from "./routes"
 import React from "react"
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   createRoutesFromElements(
     ROUTES.map(route => (
       <Route key={route.path} path={route.path} element={route.element} />
