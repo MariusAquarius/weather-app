81224b6f20937315ab9448a7b8e4780e60d4da85
Marius Unger <me@marius-unger.de>
2025-02-07 17:49:28 +0100

    * fix server & build * change postcss & tailwind config from CJS to ESM * run prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import { fontFamily } from "tailwindcss/defaultTheme"
 
-module.exports = {
+export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
   plugins: [],
   theme: {
     container: {
